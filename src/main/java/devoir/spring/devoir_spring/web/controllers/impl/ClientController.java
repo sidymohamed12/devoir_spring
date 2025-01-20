2007ec10fa2831514ff068b843b828df7c90c7ed
sidymohamed12 <mohamedsaizonou86@gmail.com>
2025-01-20 12:29:47 +0000

    first commit

--- src/main/java/devoir/spring/devoir_spring/web/controllers/impl/ClientController.java
+++ src/main/java/devoir/spring/devoir_spring/web/controllers/impl/ClientController.java
@@ -1,13 +1,11 @@
 package devoir.spring.devoir_spring.web.controllers.impl;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import devoir.spring.devoir_spring.data.entities.Commande;
 import devoir.spring.devoir_spring.services.IClientService;
 import devoir.spring.devoir_spring.services.ICommandeService;
 import devoir.spring.devoir_spring.web.controllers.IClientController;
@@ -16,7 +14,6 @@ import devoir.spring.devoir_spring.web.dto.response.ClientSimpleResponse;
 import devoir.spring.devoir_spring.web.dto.response.CommandeSimpleResponse;
 import devoir.spring.devoir_spring.web.dto.response.Result;
 import lombok.RequiredArgsConstructor;
-import lombok.val;
 
 @RestController
 @RequiredArgsConstructor
@@ -42,8 +39,10 @@ public class ClientController implements IClientController {
         var commandes = commandesPage.getContent().stream()
                 .map(CommandeSimpleResponse::new)
                 .toList();
-        var commandeResponse = commandes.stream().map(Result::new).toList();
-        return new ResponseEntity<>(commandeResponse, HttpStatus.OK);
+        Result result = new Result(client);
+        result.setCommandes(commandes);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
